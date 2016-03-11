a6b329fe1fcafca5ca6d9f4df9b4c1beadd48036
Dan Bovey <dan@danbovey.uk>
2016-03-11 00:05:58 +0000

    Converted to ES6, 14.x support, Altered scrollable element offset

--- README.md
+++ README.md
@@ -1,74 +1,68 @@
-React Infinite Scroll
-=====================
+React Infinite Scroller
+=======================
 
-*React infinite scroll component*
+Infinitely load content using a React Component. This fork maintains a simple, lightweight infinite scroll package that supports both `window` and scrollable elements.
 
-Demo: http://jsfiddle.net/mb9vJ/2
+## Version
 
-# Getting started
+0.2.0 for React 14.7
 
-### Classic :
+## Installation
 
-The "ready to use" [script file](https://raw.github.com/guillaumervls/react-infinite-scroll/master/dist/react-infinite-scroll.min.js)
-is in the `dist` folder.
-
-Then :
-```html
-<script src='react-infinite-scroll.min.js'></script>
-<script>
-  var InfiniteScroll = React.addons.InfiniteScroll;
-</script>
+```
+  npm i react-infinite-scroller
 ```
 
-### [Browserify](https://github.com/substack/node-browserify) :
-̀
-Install : `npm install react-infinite-scroll`
+## How to use
 
-Then :
-```javascript
-InfiniteScroll = require('react-infinite-scroll')(React);
+Only supports ES6 import. For other versions, please see the many forks of the [original repository](https://github.com/guillaumervls/react-infinite-scroll)
+
+```
+  import InfiniteScroll from 'react-infinite-scroller'
 ```
 
-### Also works with AMD (e.g [RequireJS](http://requirejs.org))
-
-In this case, it will depend on `react`.
-
-
-# Use in JSX
+### Window scroll events
 
 ```html
-<InfiniteScroll
-    pageStart=0
-    loadMore={loadFunc}
-    hasMore={true || false}
-    loader={<div className="loader">Loading ...</div>}
-    useWindow={true || false}>
-  {items} // <-- This is the "stuff" you want to load
-</InfiniteScroll>
+  <InfiniteScroll
+      pageStart=0
+      loadMore={loadFunc}
+      hasMore={true || false}
+      loader={<div className="loader">Loading ...</div>}>
+    {items} // <-- This is the content you want to load
+  </InfiniteScroll>
+```
+
+### DOM scroll events
+
+```
+  <div style="height:700px;overflow:auto;">
+    <InfiniteScroll
+        pageStart=0
+        loadMore={loadFunc}
+        hasMore={true || false}
+        loader={<div className="loader">Loading ...</div>}
+        useWindow={false}>
+      {items}
+    </InfiniteScroll>
+  </div>
 ```
 
 - `pageStart` : The page number corresponding to the initial `items`, defaults to `0`
                 which means that for the first loading, `loadMore` will be called with `1`
 
 - `loadMore(pageToLoad)` : This function is called when the user scrolls down
-                           and we need to load stuff
+                           and we need to load items
 
-- `hasMore` : Boolean stating if we should keep listening to scroll event and
-              trying to load more stuff
+- `hasMore` : Boolean stating whether there are more items to be loaded. Event listeners
+              are removed if `false`.
 
-- `loader` : Loader element to be displayed while loading stuff - You can use
+- `loader` : Loader element to be displayed while loading items - You can use
              `InfiniteScroll.setDefaultLoader(loader);` to set a defaut loader
              for all your `InfiniteScroll` components
 
 - `threshold` : The distance between the bottom of the page and the bottom of the
-                window's viewport that triggers the loading of new stuff -
+                window's viewport that triggers the loading of new items -
                 *Defaults to `250`*
 
 - `useWindow` : Boolean stating whether to add listeners to the window, or else, the DOMNode
-
-## (Re)build
-
-```
-npm install
-grunt dist
-```
--- src/react-infinite-scroll.js
+++ src/react-infinite-scroll.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 function topPosition(domElt) {
   if (!domElt) {
     return 0;
@@ -5,78 +8,85 @@ function topPosition(domElt) {
   return domElt.offsetTop + topPosition(domElt.offsetParent);
 }
 
-module.exports = function (React) {
-  if (React.addons && React.addons.InfiniteScroll) {
-    return React.addons.InfiniteScroll;
+var component;
+
+export default class InfiniteScroll extends React.Component {
+  constructor(props) {
+    super(props);
+
+    component = this;
   }
-  React.addons = React.addons || {};
-  var InfiniteScroll = React.addons.InfiniteScroll = React.createClass({
-    getDefaultProps: function () {
-      return {
-        pageStart: 0,
-        hasMore: false,
-        loadMore: function () {},
-        threshold: 250
-      };
-    },
-    componentDidMount: function () {
-      this.pageLoaded = this.props.pageStart;
-      this.attachScrollListener();
-    },
-    componentDidUpdate: function () {
-      this.attachScrollListener();
-    },
-    render: function () {
-      var props = this.props;
-      return React.DOM.div(null, props.children, props.hasMore && (props.loader || InfiniteScroll._defaultLoader));
-    },
-    scrollListener: function () {
-      var el = this.getDOMNode();
-      var scrollEl = window;
+  componentDidMount() {
+    this.pageLoaded = this.props.pageStart;
+    this.attachScrollListener();
+  }
+  componentDidUpdate() {
+    this.attachScrollListener();
+  }
+  render() {
+    var props = this.props;
+    return React.DOM.div(null, props.children, props.hasMore && (props.loader || this._defaultLoader));
+  }
+  scrollListener() {
+    var el = ReactDOM.findDOMNode(component);
+    var scrollEl = window;
 
+    var offset;
+    if(component.props.useWindow == true) {
       var scrollTop = (scrollEl.pageYOffset !== undefined) ? scrollEl.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-      var offset = topPosition(el) + el.offsetHeight - scrollTop - window.innerHeight;
-      if(this.props.useWindow == false) {
-        scrollEl = el.parentNode;
-        offset = el.getBoundingClientRect().top + el.offsetHeight - el.parentNode.clientHeight;
-      }
-
-      if (offset < Number(this.props.threshold)) {
-        this.detachScrollListener();
-        // call loadMore after detachScrollListener to allow
-        // for non-async loadMore functions
-        this.props.loadMore(this.pageLoaded += 1);
-      }
-    },
-    attachScrollListener: function () {
-      if (!this.props.hasMore) {
-        return;
-      }
-
-      var scrollEl = window;
-      if(this.props.useWindow == false) {
-        scrollEl = this.getDOMNode().parentNode;
-      }
-
-      scrollEl.addEventListener('scroll', this.scrollListener);
-      scrollEl.addEventListener('resize', this.scrollListener);
-      this.scrollListener();
-    },
-    detachScrollListener: function () {
-      var scrollEl = window;
-      if(this.props.useWindow == false) {
-        scrollEl = this.getDOMNode().parentNode;
-      }
-
-      scrollEl.removeEventListener('scroll', this.scrollListener);
-      scrollEl.removeEventListener('resize', this.scrollListener);
-    },
-    componentWillUnmount: function () {
-      this.detachScrollListener();
+      offset = topPosition(el) + el.offsetHeight - scrollTop - height;
+    } else {
+      offset = el.offsetHeight - el.parentNode.scrollTop - el.parentNode.clientHeight;
     }
-  });
-  InfiniteScroll.setDefaultLoader = function (loader) {
-    InfiniteScroll._defaultLoader = loader;
-  };
-  return InfiniteScroll;
+
+    if (offset < Number(component.props.threshold)) {
+      component.detachScrollListener();
+      // call loadMore after detachScrollListener to allow
+      // for non-async loadMore functions
+      component.props.loadMore(component.pageLoaded += 1);
+    }
+  }
+  attachScrollListener() {
+    if (!component.props.hasMore) {
+      return;
+    }
+
+    var scrollEl = window;
+    if(component.props.useWindow == false) {
+      scrollEl = ReactDOM.findDOMNode(this).parentNode;
+    }
+
+    scrollEl.addEventListener('scroll', this.scrollListener);
+    scrollEl.addEventListener('resize', this.scrollListener);
+    this.scrollListener();
+  }
+  detachScrollListener() {
+    var scrollEl = window;
+    if(component.props.useWindow == false) {
+      scrollEl = ReactDOM.findDOMNode(this).parentNode;
+    }
+
+    scrollEl.removeEventListener('scroll', this.scrollListener);
+    scrollEl.removeEventListener('resize', this.scrollListener);
+  }
+  componentWillUnmount() {
+    this.detachScrollListener();
+  }
+  setDefaultLoader(loader) {
+    this._defaultLoader = loader;
+  }
+}
+InfiniteScroll.PropTypes = {
+  pageStart: React.PropTypes.number,
+  hasMore: React.PropTypes.bool,
+  loadMore: React.PropTypes.func.isRequired,
+  threshold: React.PropTypes.number,
+  useWindow: React.PropTypes.bool
+}
+InfiniteScroll.defaultProps = {
+  pageStart: 0,
+  hasMore: false,
+  loadMore: function () {},
+  threshold: 250,
+  useWindow: true
 };
