3f1598a102f8048cc0fd7b7dc7523ae499780c6a
Jon Deibel <jon_deibel@intuit.com>
2017-03-22 14:20:37 -0700

    Offset calculation ensures element is visible

--- src/InfiniteScroll.js
+++ src/InfiniteScroll.js
@@ -95,7 +95,8 @@ export default class InfiniteScroll extends Component {
                 offset = el.scrollHeight - parentNode.scrollTop - parentNode.clientHeight;
         }
 
-        if(offset < Number(this.props.threshold)) {
+        // Here we make sure the element is visible as well as checking the offset
+        if(offset < Number(this.props.threshold) && el.offsetParent !== null``) {
             this.detachScrollListener();
             // Call loadMore after detachScrollListener to allow for non-async loadMore functions
             if(typeof this.props.loadMore == 'function') {
