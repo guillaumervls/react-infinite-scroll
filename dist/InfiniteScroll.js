4b9c05184e8d49562467633393f4b107c7415aac
Gubsz <dlstanle@gmail.com>
2017-06-07 23:40:32 -0700

    Forgot to build the project

--- dist/InfiniteScroll.js
+++ dist/InfiniteScroll.js
@@ -149,7 +149,12 @@ var InfiniteScroll = function (_Component) {
         _this2.scrollComponent = node;
       };
 
-      return _react2.default.createElement(element, props, children, hasMore && (loader || this.defaultLoader));
+      var childElements = [children, hasMore && (loader || this.defaultLoader)];
+      if (isReverse) {
+        childElements = childElements.reverse();
+      }
+
+      return _react2.default.createElement(element, props, childElements);
     }
   }]);
 
